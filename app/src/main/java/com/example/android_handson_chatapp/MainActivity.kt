d1c8a20bff95136244d4cbebde92eadf1c225f26
syota-kawaguchi <57338033+syota-kawaguchi@users.noreply.github.com>
2022-02-16 15:42:53 +0900

    binding対応

--- app/src/main/java/com/example/android_handson_chatapp/MainActivity.kt
+++ app/src/main/java/com/example/android_handson_chatapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.android_handson_chatapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.android_handson_chatapp.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        val view = binding.root
+
+        setContentView(view)
     }
 }
