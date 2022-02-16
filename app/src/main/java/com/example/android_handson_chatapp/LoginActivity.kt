0ce86ae17f0a26ca56844fe1539284c7c53a828f
syota-kawaguchi <57338033+syota-kawaguchi@users.noreply.github.com>
2022-02-16 19:11:01 +0900

    ログイン画面の実装(機能は登録画面にもどるのみ)

---
+++ app/src/main/java/com/example/android_handson_chatapp/LoginActivity.kt
@@ -0,0 +1,26 @@
+package com.example.android_handson_chatapp
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.android_handson_chatapp.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+         binding = ActivityLoginBinding.inflate(layoutInflater)
+         setContentView(binding.root)
+
+        binding.backToRegisterTextLogin.setOnClickListener {
+            finish()
+        }
+    }
+}
