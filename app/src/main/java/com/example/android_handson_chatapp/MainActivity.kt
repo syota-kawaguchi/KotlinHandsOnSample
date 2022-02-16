9ee0777a03c71718593d968a3d5dc8f440639d9b
syota-kawaguchi <57338033+syota-kawaguchi@users.noreply.github.com>
2022-02-16 19:13:39 +0900

    登録画面からログイン画面への繊維機能の実装

--- app/src/main/java/com/example/android_handson_chatapp/MainActivity.kt
+++ app/src/main/java/com/example/android_handson_chatapp/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.android_handson_chatapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.android_handson_chatapp.databinding.ActivityMainBinding
 
 private lateinit var binding: ActivityMainBinding
+
+private val tag = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +19,20 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
 
         setContentView(view)
+
+        binding.registerButtonRegister.setOnClickListener {
+            val email = binding.emailEdittextRegister.text.toString();
+            val password = binding.passwordEdittextRegister.text.toString();
+
+            Log.d(tag, "Email is: ${email}")
+            Log.d(tag, "password is: ${password}")
+        }
+
+        binding.haveAccountTextRegister.setOnClickListener {
+            Log.d(tag, "Try show login Activity")
+
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
