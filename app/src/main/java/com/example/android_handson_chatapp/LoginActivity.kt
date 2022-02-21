925b4abf169f405a83d02c2070a40fda2bfecfeb
syota-kawaguchi <57338033+syota-kawaguchi@users.noreply.github.com>
2022-02-21 22:54:47 +0900

    Login処理の追加(Login後の画面遷移はまだ)

--- app/src/main/java/com/example/android_handson_chatapp/LoginActivity.kt
+++ app/src/main/java/com/example/android_handson_chatapp/LoginActivity.kt
@@ -1,19 +1,48 @@
 package com.example.android_handson_chatapp
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import com.example.android_handson_chatapp.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+
+    private val tag = "LOGIN"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
          binding = ActivityLoginBinding.inflate(layoutInflater)
          setContentView(binding.root)
+
+        binding.loginButtonLogin.setOnClickListener{
+            val email = binding.emailEdittextLogin.text.toString()
+            val password = binding.passwordEdittextLogin.text.toString()
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Please enter text in email or password", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            Log.d(tag, "Attempt login with email and password email:${email} password:${password}")
+
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    if (!it.isSuccessful) {
+                        Toast.makeText(this, "Failed to Login", Toast.LENGTH_SHORT).show()
+                    }
+
+                    Log.d(tag, "Successful Login")
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this, "Failed to Login", Toast.LENGTH_SHORT).show()
+                }
+        }
 
         binding.backToRegisterTextLogin.setOnClickListener {
             finish()
