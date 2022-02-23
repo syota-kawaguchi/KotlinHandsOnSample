28122d386fb6cdccf595a148a2dc16228c5526e5
syota-kawaguchi <57338033+syota-kawaguchi@users.noreply.github.com>
2022-02-24 00:35:48 +0900

    Implement LatestMessageActivity fetchUser

--- app/src/main/java/com/example/android_handson_chatapp/LatestMessageActivity.kt
+++ app/src/main/java/com/example/android_handson_chatapp/LatestMessageActivity.kt
@@ -3,16 +3,49 @@ package com.example.android_handson_chatapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android_handson_chatapp.databinding.ActivityLatestMessageBinding
+import com.example.android_handson_chatapp.databinding.ActivityRegisterBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class LatestMessageActivity : AppCompatActivity() {
+
+    companion object {
+        var currentuser: User? = null
+        val TAG = "LatestMessagesActivity"
+        var recyclerView: RecyclerView? =null
+    }
+
+    private lateinit var binding : ActivityLatestMessageBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_latest_message)
 
+        binding = ActivityLatestMessageBinding.inflate(layoutInflater)
+
+        val view = binding.root
+
+        setContentView(view)
+
+        recyclerView = binding.RecyclerViewLatestMessage
+
+        //ログインされているかの判定
         verifyUserIsLoggedIn()
+
+        //ログイン情報の取得
+        fetchCurrentUser()
+
+        //データの取得
+        listenForLatestMessages()
     }
 
     private fun verifyUserIsLoggedIn() {
@@ -23,6 +56,44 @@ class LatestMessageActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
+    private fun fetchCurrentUser() {
+        val ref = FirebaseDatabase.getInstance().getReference("/users/")
+
+        ref.addListenerForSingleValueEvent(object : ValueEventListener {
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                var items = mutableListOf<LatestMessageItem>()
+
+                snapshot.children.forEach {
+                    Log.d(TAG, "snapshot children : ${it.toString()}")
+                    val user = it.getValue(User::class.java)
+                    if (user != null) {
+                        items.add(LatestMessageItem(user.username, "", user.profileImageUrl))
+                    }
+                }
+
+                recyclerView?.apply {
+                    setHasFixedSize(true)
+                    layoutManager = LinearLayoutManager(context)
+                    adapter = LatestMessageAdaptor(
+                        items,
+                        object : LatestMessageAdaptor.ListListener {
+                            override fun onClickItem(tappedView: View, latestMessageItem: LatestMessageItem) {}
+                        }
+                    )
+                }
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+//                TODO("Not yet implemented")
+            }
+        })
+    }
+
+    private fun listenForLatestMessages() {}
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item?.itemId == R.id.menu_sign_out){
