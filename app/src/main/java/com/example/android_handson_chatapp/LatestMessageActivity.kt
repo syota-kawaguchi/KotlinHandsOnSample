1c864735ac9996f82fd58c4c0ddf2549ad6e1e12
Juris710 <juris710.dev@gmail.com>
2022-03-01 21:45:12 +0900

    リファクタリング

--- app/src/main/java/com/example/android_handson_chatapp/LatestMessageActivity.kt
+++ app/src/main/java/com/example/android_handson_chatapp/LatestMessageActivity.kt
@@ -7,16 +7,32 @@ import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android_handson_chatapp.databinding.ActivityLatestMessageBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
+
+suspend fun DatabaseReference.awaitGet(): DataSnapshot {
+    return withContext(Dispatchers.IO) {
+        suspendCoroutine { continuation ->
+            get().addOnSuccessListener {
+                continuation.resume(it)
+            }.addOnFailureListener {
+                continuation.resumeWithException(it)
+            }
+        }
+    }
+}
 
 class LatestMessageActivity : AppCompatActivity() {
 
@@ -24,10 +40,10 @@ class LatestMessageActivity : AppCompatActivity() {
         var currentuser: User? = null
         val USER_KEY = "USER_KEY"
         val TAG = "LatestMessagesActivity"
-        var recyclerView: RecyclerView? =null
+        var recyclerView: RecyclerView? = null
     }
 
-    private lateinit var binding : ActivityLatestMessageBinding
+    private lateinit var binding: ActivityLatestMessageBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,7 +56,8 @@ class LatestMessageActivity : AppCompatActivity() {
 
         recyclerView = binding.RecyclerViewLatestMessage
 
-        val dividerItemDecoration = DividerItemDecoration(this, LinearLayoutManager(this).orientation)
+        val dividerItemDecoration =
+            DividerItemDecoration(this, LinearLayoutManager(this).orientation)
         recyclerView?.addItemDecoration(dividerItemDecoration)
 
         //ログインされているかの判定
@@ -81,67 +98,35 @@ class LatestMessageActivity : AppCompatActivity() {
         val fromId = FirebaseAuth.getInstance().uid
         val ref = FirebaseDatabase.getInstance().getReference("/users/")
 
-        ref.addListenerForSingleValueEvent(object : ValueEventListener {
-
-            override fun onDataChange(snapshot: DataSnapshot) {
-
-                var items = mutableListOf<LatestMessageItem>()
-
-                snapshot.children.forEach {
-                    Log.d(TAG, "snapshot children : ${it.toString()}")
-                    val user = it.getValue(User::class.java)
-                    if (user != null) {
-                        /*
-                        * TODO
-                        *  userの情報取得する
-                        *  最新のメッセージを参照・あれば取得する <- ここをコルーチンにしたい
-                        *  メッセージを取得後アイテムを配列に追加
-                        *  childrenの要素数回繰り返す
-                        *  すべての処理が終わったらAdapterに追加
-                        * */
-                        val ref = FirebaseDatabase.getInstance().getReference("/latest-messages/$fromId/${user.uid}")
-                        ref.get().addOnSuccessListener {
-                            Log.d(TAG, "success load latest message : ${it.key}")
-                            val chatMessage = it.getValue(ChatMessage::class.java)
-                            if (chatMessage == null) {
-                                Log.d(TAG, "Latest Message is blank : ${chatMessage}")
-                                items.add(LatestMessageItem(user, ""))
-                                refreshRecycleView(items)
-                            }
-                            else {
-                                Log.d(TAG, "chatMessage is : ${chatMessage.text}")
-                                items.add(LatestMessageItem(user, chatMessage.text))
-                                refreshRecycleView(items)
-                            }
-                        }
-                        .addOnFailureListener {
-                            Log.d(TAG, "failed to load latest message : ${it.message}")
-                        }
-                        //=====================================================================================================
-                    }
-                }
+        lifecycleScope.launch {
+            val usersSnapshot = ref.awaitGet()
+            val latestMessageItems = usersSnapshot.children.mapNotNull { userSnapshot ->
+                val user = userSnapshot.getValue(User::class.java) ?: return@mapNotNull null
+                val latestMessageRef = FirebaseDatabase.getInstance()
+                    .getReference("/latest-messages/$fromId/${user.uid}")
+                val latestMessageSnapshot = latestMessageRef.awaitGet()
+                val latestMessage = latestMessageSnapshot.getValue(ChatMessage::class.java)
+                val message = latestMessage?.text ?: ""
+                LatestMessageItem(user, message)
             }
-
-            override fun onCancelled(error: DatabaseError) {
-//                TODO("Not yet implemented")
-            }
-        })
+            refreshRecycleView(latestMessageItems)
+        }
     }
 
-    private suspend fun fetchLatestMessage(fromId: String, toId: String) : String {
+    private suspend fun fetchLatestMessage(fromId: String, toId: String): String {
         val task = GlobalScope.async(Dispatchers.IO) {
-            val ref = FirebaseDatabase.getInstance().getReference("/latest-messages/$fromId/${toId}")
+            val ref =
+                FirebaseDatabase.getInstance().getReference("/latest-messages/$fromId/${toId}")
             ref.get().addOnSuccessListener {
                 if (it.exists()) {
                     it.getValue(LatestMessageItem::class.java)
-                }
-                else {
+                } else {
                     ""
                 }
             }
-            .addOnFailureListener {
-                ""
-            }
+                .addOnFailureListener {
+                    ""
+                }
         }
         return withContext(Dispatchers.Main) {
             val message = task.await()
@@ -150,14 +135,17 @@ class LatestMessageActivity : AppCompatActivity() {
         }
     }
 
-    private fun refreshRecycleView(items: MutableList<LatestMessageItem>) {
+    private fun refreshRecycleView(items: List<LatestMessageItem>) {
         recyclerView?.apply {
             setHasFixedSize(true)
             layoutManager = LinearLayoutManager(context)
             adapter = LatestMessageAdaptor(
                 items,
                 object : LatestMessageAdaptor.ListListener {
-                    override fun onClickItem(tappedView: View, latestMessageItem: LatestMessageItem) {
+                    override fun onClickItem(
+                        tappedView: View,
+                        latestMessageItem: LatestMessageItem
+                    ) {
                         val intent = Intent(tappedView.context, ChatLogActivity::class.java)
                         intent.putExtra(USER_KEY, latestMessageItem.user)
                         startActivity(intent)
@@ -168,7 +156,7 @@ class LatestMessageActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item?.itemId == R.id.menu_sign_out){
+        if (item?.itemId == R.id.menu_sign_out) {
             FirebaseAuth.getInstance().signOut()
             val intent = Intent(this, RegisterActivity::class.java)
             intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
